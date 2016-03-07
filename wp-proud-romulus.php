c24b7445d169180d1ac8fe90df9035d8475484d1
Jeff Lyon <jeff@albatrossdigital.com>
2016-03-07 11:50:33 -0800

    Initial commit

---
+++ wp-proud-romulus.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Proud Romulus
+Plugin URI: http://proudcity.com/
+Description: Integrates romulusapp.com with ProudCity
+Version: 1.1.0
+Author: ProudCity
+Author URI: https://proudcity.com/
+License: Affero GPL v2
+*/
+
+namespace Proud\Romulus;
+
+
+class Romulus extends \ProudPlugin {
+
+
+  public function __construct() {
+
+  }
+
+
+} // class
+
+
+new ProudPayment;
