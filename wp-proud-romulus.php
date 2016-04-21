b7267591bd067a30c66d4f070bf2b71d8ada7bf7
Jeff Lyon <jeff@albatrossdigital.com>
2016-04-21 07:24:47 -0700

    Building romulus widget

--- wp-proud-romulus.php
+++ wp-proud-romulus.php
@@ -15,12 +15,69 @@ namespace Proud\Romulus;
 class Romulus extends \ProudPlugin {
 
 
-  public function __construct() {
+  function __construct() {
+    parent::__construct(
+      'proud_romulus', // Base ID
+      __( 'Romulus From', 'wp-proud-romulus' ), // Name
+      array( 'description' => __( 'Romulus Contact From', 'wp-proud-romulus' ), ) // Args
+    );
+  }
 
+  function initialize() {
+    //$forms = RGFormsModel::get_forms( 1, 'title' );
+    // @todo: get from Romulus API (does it exist?)
+    $options = [
+      'ward-1', 'Ward 1',
+      'ward-2', 'Ward 2',
+      'ward-3', 'Ward 3',
+      'ward-4', 'Ward 4',
+      'ward-5', 'Ward 5',
+    ];
+    $default = 'ward-1';
+    $this->settings = [
+      'department' => [
+        '#title' => 'Department',
+        '#type' => 'select',
+        '#default_value' => $default,
+        '#to_js_settings' => false,
+        '#options' => $options
+      ],
+    ];
+  }
+
+
+  /**
+   * Determines if content empty, show widget, title ect?  
+   *
+   * @see self::widget()
+   *
+   * @param array $args     Widget arguments.
+   * @param array $instance Saved values from database.
+   */
+  public function hasContent($args, &$instance) {
+    return true;
+  }
+
+  /**
+   * Front-end display of widget.
+   *
+   * @see WP_Widget::widget()
+   *
+   * @param array $args     Widget arguments.
+   * @param array $instance Saved values from database.
+   */
+  public function printWidget( $args, $instance ) {
+    extract($instance);
+    $city = get_option('proud_romulus_city', 'demo-city-216');
+    ?>
+    <iframe src="https://romulusapp.com/contact/<?php print $city; ?>/<?php print $department; ?>"
+            width="519"
+            height="846"></iframe>
+    <?php
   }
 
 
 } // class
 
 
-new ProudPayment;
+new Romulus;
